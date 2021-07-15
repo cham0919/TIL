e34bf9717198325bf41f3396c7846c21f9f12ec4
ckadl0919 <ckadl0118@gmail.com>
2021-07-15 11:59:53 +0900

    feat. READMEUpade 선언
     READMEUpade 선언

---
+++ src/tree/ReadMeUpdate.java
@@ -0,0 +1,16 @@
+package tree;
+
+import java.io.File;
+
+public class ReadMeUpdate {
+
+    public static void main(String[] args) {
+        ReadMeUpdate readMeUpdate = new ReadMeUpdate();
+        readMeUpdate.readMeUpdate();
+    }
+
+    public void readMeUpdate(){
+        File file = new File("./README.md");
+        
+    }
+}
