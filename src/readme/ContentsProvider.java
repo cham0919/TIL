6c82ff4f7ed61ddd6fce3e070d658103cd83a3c5
ckadl0919 <ckadl0118@gmail.com>
2021-08-06 02:30:48 +0900

    feat. 디렉토리 내부 파일 정렬 알고리즘 추가
    디렉토리 내부 파일 정렬 알고리즘 추가

--- src/readme/ContentsProvider.java
+++ src/readme/ContentsProvider.java
@@ -2,8 +2,16 @@ package readme;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.Comparator;
+
 
 public class ContentsProvider {
+
+    Comparator<File> fileComparator = (o1, o2) -> {
+        if (o1.isDirectory() && !o2.isDirectory()) return -1;
+        else return o1.compareTo(o2);
+    };
 
     public static void main(String[] args) throws IOException {
         ContentsProvider provider = new ContentsProvider();
@@ -21,7 +29,8 @@ public class ContentsProvider {
         StringBuilder sb = new StringBuilder(ReadMeFormat.HEADER.getLine(null));
         for (File file : files) {
             sb.append(dirToContent(file));
-            dfsFiles(sb, file.listFiles(), "");
+            File[] sortedFiles = Arrays.stream(file.listFiles()).sorted(fileComparator).toArray(File[]::new);
+            dfsFiles(sb, sortedFiles, "");
         }
         return sb.toString();
     }
@@ -35,7 +44,8 @@ public class ContentsProvider {
     private void dfsFile(StringBuilder sb, File file, String prefix) {
         if (file.isDirectory()) {
             sb.append(subDirToContent(file));
-            dfsFiles(sb, file.listFiles(), prefix + "  ");
+            File[] files = Arrays.stream(file.listFiles()).sorted(fileComparator).toArray(File[]::new);
+            dfsFiles(sb, files, prefix + "  ");
         } else {
             sb.append(fileToContent(file, prefix));
         }
--- src/readme/TILFileFetcher.java
+++ src/readme/TILFileFetcher.java
@@ -2,6 +2,7 @@ package readme;
 
 import java.io.File;
 import java.util.Arrays;
+import java.util.Comparator;
 import java.util.function.BiPredicate;
 import java.util.function.Predicate;
 
@@ -10,20 +11,19 @@ public class TILFileFetcher {
     Predicate<File> checkDir = (file) -> file.isDirectory();
     Predicate<String> checkTILDirName = (name) -> isDirName(name);
     BiPredicate<File, String> checkTILDir = (file, name) -> checkDir.test(file) && checkTILDirName.test(name);
-
-    public File[] getFiles(){
-        File TILDir =  new File("./");
-        File[] files = getDirArray(TILDir);
-        return Arrays.stream(files).sorted((o1, o2) -> compateToFilePrefixNumber(o1, o2))
-        .toArray(File[]::new);
-    }
-
-    private int compateToFilePrefixNumber(File o1, File o2){
+    Comparator<File> compateToFilePrefixNumber = (o1, o2) -> {
         String o1Name = o1.getName();
         String o2Name = o2.getName();
         return Integer.valueOf(o1Name.substring(0, o1Name.indexOf("."))).compareTo(
                 Integer.valueOf(o2Name.substring(0, o2Name.indexOf(".")))
         );
+    };
+
+    public File[] getFiles(){
+        File TILDir =  new File("./");
+        File[] files = getDirArray(TILDir);
+        return Arrays.stream(files).sorted(compateToFilePrefixNumber)
+        .toArray(File[]::new);
     }
 
     private File[] getDirArray(File file) {
